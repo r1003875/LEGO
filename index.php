e7ef96a40821419599f844ce7937666bc68d7468
r1003875 <147533527+r1003875@users.noreply.github.com>
2024-10-14 13:02:05 +0200

    feat: include db_products

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
     if($_SESSION['loggedin'] !== true){
         header('Location: login.php');
     }
+    $conn = new PDO('mysql:dbname=legoshop;host=localhost', "root", "");
+    $statement = $conn->prepare('SELECT * FROM products');
+    $statement->execute();
+    $products = $statement->fetchall(PDO::FETCH_ASSOC);
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,58 +21,21 @@
 <body>
     <?php include_once("nav.inc.php"); ?>
     <main class="shopping_page">
+    <?php foreach($products as $p): ?>
         <article>
             <div>
-                <img src="images/notre_dame.png" alt="notre dame">
+                <img src=<?php echo $p['image']?> alt="notre dame">
             </div>
             <div class="span_div">
-                <span class="age"><img src="images/cake-candles-solid.svg" alt="age" class="icon"> 18+</span>
-                <span class="pieces"><img src="images/puzzle-piece-solid.svg" alt="pieces" class="icon"> 4862</span>
-                <span class="rating"><img src="images/star-solid.svg" alt="rating" class="icon"> 5.0</span>
+                <span class="age"><img src="images/cake-candles-solid.svg" alt="age" class="icon"> <?php echo $p['min_age'] ?>+</span>
+                <span class="pieces"><img src="images/puzzle-piece-solid.svg" alt="pieces" class="icon"> <?php echo $p['pieces_amount'] ?></span>
+                <span class="rating"><img src="images/star-solid.svg" alt="rating" class="icon"> <?php echo $p['rating'] ?></span>
             </div>
-            <h4>Lego City - Brandweer</h4>
-            <span class="price">$299.99</span>
+            <h4><?php echo $p['name'] ?></h4>
+            <span class="price">$<?php echo $p['price'] ?></span>
             <div class="btn2">Add to cart</div>
-        </article> 
-        <article>
-            <div>
-                <img src="images/notre_dame.png" alt="notre dame">
-            </div>
-            <div class="span_div">
-                <span class="age"><img src="images/cake-candles-solid.svg" alt="age" class="icon"> 18+</span>
-                <span class="pieces"><img src="images/puzzle-piece-solid.svg" alt="pieces" class="icon"> 4862</span>
-                <span class="rating"><img src="images/star-solid.svg" alt="rating" class="icon"> 5.0</span>
-            </div>
-            <h4>Lego City - Brandweer</h4>
-            <span class="price">$299.99</span>
-            <div class="btn2">Add to cart</div>
-        </article>     
-        <article>
-            <div>
-                <img src="images/notre_dame.png" alt="notre dame">
-            </div>
-            <div class="span_div">
-                <span class="age"><img src="images/cake-candles-solid.svg" alt="age" class="icon"> 18+</span>
-                <span class="pieces"><img src="images/puzzle-piece-solid.svg" alt="pieces" class="icon"> 4862</span>
-                <span class="rating"><img src="images/star-solid.svg" alt="rating" class="icon"> 5.0</span>
-            </div>
-            <h4>Lego City - Brandweer</h4>
-            <span class="price">$299.99</span>
-            <div class="btn2">Add to cart</div>
-        </article>     
-        <article>
-            <div>
-                <img src="images/notre_dame.png" alt="notre dame">
-            </div>
-            <div class="span_div">
-                <span class="age"><img src="images/cake-candles-solid.svg" alt="age" class="icon"> 18+</span>
-                <span class="pieces"><img src="images/puzzle-piece-solid.svg" alt="pieces" class="icon"> 4862</span>
-                <span class="rating"><img src="images/star-solid.svg" alt="rating" class="icon"> 5.0</span>
-            </div>
-            <h4>Lego City - Brandweer</h4>
-            <span class="price">$299.99</span>
-            <div class="btn2">Add to cart</div>
-        </article>           
+        </article>  
+    <?php endforeach; ?>        
     </main>
 
 </body>
