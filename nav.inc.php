f5621239f380613cce067560a0999f215f685386
r1003875 <147533527+r1003875@users.noreply.github.com>
2024-11-19 16:57:17 +0100

    fix: admin page is now hidden for customers

--- nav.inc.php
+++ nav.inc.php
@@ -3,10 +3,10 @@
 ?><nav>
     <div class="primary_links">
         <a href="index.php"><img src="images/LEGO_logo.png" alt="logo" class="logo"></a>
-        <a href="">SHOP</a>
+        <a href="index.php">SHOP</a>
         <a href="">DISCOVER</a>
         <a href="">HELP</a>
-        <a href="new.php" class="highlighted">NEW</a>
+        <a href="new.php" class="highlighted <?php if(!$user->canAddProduct()){echo "hidden";} ?>">NEW</a>
     </div>
     <div class="secondary_links">
         <input type="text" placeholder="Search..." class="search_input">
