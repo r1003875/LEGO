3720d829d62d1cd4a59d0808be4f53c6303d5437
r1003875 <147533527+r1003875@users.noreply.github.com>
2024-11-19 16:31:38 +0100

    feat: username displays in nav

--- nav.inc.php
+++ nav.inc.php
@@ -1,4 +1,6 @@
-<nav>
+<?php 
+    include_once("classes/User.php"); 
+?><nav>
     <div class="primary_links">
         <a href="index.php"><img src="images/LEGO_logo.png" alt="logo" class="logo"></a>
         <a href="">SHOP</a>
@@ -9,7 +11,7 @@
     <div class="secondary_links">
         <input type="text" placeholder="Search..." class="search_input">
         <a href=""><img src="images/bag-shopping-solid.svg" alt="cart" class="shopping_bag"></a>
-        <a href="">ACCOUNT</a>
+        <a href=""><?php echo htmlspecialchars(strtoupper($user->getFirstname())); ?></a>
         <a href="logout.php">LOG OUT</a>
     </div>
 </nav>
